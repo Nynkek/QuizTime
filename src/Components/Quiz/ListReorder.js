1e39fe244411ba23b8bf76b6d339273d1a462920
Pim <pim@pixelrock.nl>
2024-09-22 14:37:12 +0200

    Updated ListReorder logic!

--- src/Components/Quiz/ListReorder.js
+++ src/Components/Quiz/ListReorder.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+import { ScoreContext } from '../../Context/ScoreProvider';
+import { useNavigate } from 'react-router-dom';
 
 import './Quiz.css';
 
@@ -9,37 +11,46 @@ const questions_drag_and_drop = [
         focused_word: 'Lengte',
         type: 'drag_and_drop',
         question_options: ['Nynke', 'Margriet', 'Jan', 'Teun'],
-        answer_ordered: ['bruin', 'blauw', 'paars', 'groen'],
+        answer_order_indices: [3, 2, 1, 0],
         options_measurement: ['Langst', 'Kortst']
     },
     {
         title: 'Wie is er het langst',
         focused_word: 'samen',
         type: 'drag_and_drop',
-        question_options: ['Nynke & Nynke', 'Bugs & Lola', 'Jan & Hannie', 'Corrie & Koos'],
-        answer_ordered: ['Nynke & Nynke', 'Bugs & Lola', 'Jan & Hannie', 'Corrie & Koos'],
+        question_options: ['Pim & Nynke', 'Bugs & Lola', 'Jan & Hannie', 'Corrie & Koos'],
+        answer_order_indices: [3, 2, 1, 0],
         options_measurement: ['Langst', 'Kortst']
-    },
+    }
     // Voeg hier meer vragen toe
 ];
 
 function ListReorder() {
-    const [answers, setAnswers] = useState([]);  // Houd de antwoorden bij
-    const [visibleQuestions, setVisibleQuestions] = useState([0]);  // Houd bij welke vragen zichtbaar zijn
+    const [answers, setAnswers] = useState([]); // Houd de antwoorden bij
+    const [visibleQuestions, setVisibleQuestions] = useState([0]); // Houd bij welke vragen zichtbaar zijn
     const [itemsList, setItemsList] = useState(
-        questions_drag_and_drop.map((question) => question.question_options)
+        questions_drag_and_drop.map(question => question.question_options)
     ); // Houd de items bij voor elke vraag
+    const [allAnswered, setAllAnswered] = useState(false);
+    const [errors, setErrors] = useState([]);
+    const { score, setScore } = useContext(ScoreContext);
+    const navigate = useNavigate();
 
-    const handleAnswerSelection = (questionIndex, selectedAnswer) => {
-        const updatedAnswers = [...answers];
-        updatedAnswers[questionIndex] = selectedAnswer;
+    const handleAnswerSelection = (questionIndex, order) => {
+        const updatedAnswers = answers;
+        const options = questions_drag_and_drop[questionIndex].question_options;
+        const answerIndices = order.map(answer => options.indexOf(answer));
+        updatedAnswers[questionIndex] = answerIndices;
         setAnswers(updatedAnswers);
 
         // Voeg de volgende vraag toe aan de zichtbare vragen als deze er is
-        if (questionIndex < questions_drag_and_drop.length - 1) {
+        if (
+            questionIndex === visibleQuestions.length - 1 &&
+            visibleQuestions.length < questions_drag_and_drop.length
+        ) {
             setVisibleQuestions([...visibleQuestions, questionIndex + 1]);
-        } else {
-            console.log('Alle vragen zijn beantwoord');
+        } else if (questionIndex === questions_drag_and_drop.length - 1) {
+            setAllAnswered(true);
         }
     };
 
@@ -70,33 +81,86 @@ function ListReorder() {
         handleAnswerSelection(questionIndex, updatedItems);
     };
 
+    const arraysEqual = (left, right) => {
+        if (left.length !== right.length) {
+            return false;
+        }
+        for (let index = 0; index < left.length; index++) {
+            if (left[index] !== right[index]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const checkAnswers = () => {
+        let tempScore = score;
+        const newErrors = [];
+
+        questions_drag_and_drop.forEach((question, index) => {
+            const userAnswers = answers[index] || [];
+
+            // Controleer of de gegeven antwoorden overeenkomen met de correcte antwoorden
+            if (!arraysEqual(userAnswers, question.answer_order_indices)) {
+                newErrors[index] = true;
+                tempScore -= 5;
+            } else {
+                newErrors[index] = false;
+            }
+        });
+
+        setErrors(newErrors);
+
+        const allAnsweredCorrectly = newErrors.every(error => error === false);
+        if (allAnsweredCorrectly) {
+            tempScore += questions_drag_and_drop.length * 10; // +10 per goed antwoord
+        }
+        setScore(tempScore);
+
+        if (allAnsweredCorrectly) {
+            navigate('/quiz4');
+        }
+    };
+
     return (
-        <div className='quiz-drag-and-drop-list'>
-            {visibleQuestions.map((questionIndex) => {
+        <div className="quiz-drag-and-drop-list">
+            {visibleQuestions.map(questionIndex => {
                 const currentQuestion = questions_drag_and_drop[questionIndex];
                 const items = itemsList[questionIndex];
 
                 return (
-                    <div key={questionIndex} className='quiz-container'>
-                        <div className='question'>
+                    <div key={questionIndex} className="quiz-container">
+                        <div className="question">
                             <h3>{currentQuestion.title}</h3>
-                            <h3><strong>{currentQuestion.focused_word}</strong></h3>
-                            <DragDropContext onDragEnd={(result) => onDragEnd(result, questionIndex)}>
+                            <h3>
+                                <strong>{currentQuestion.focused_word}</strong>
+                            </h3>
+                            <DragDropContext onDragEnd={result => onDragEnd(result, questionIndex)}>
                                 <Droppable droppableId={`droppable-${questionIndex}`}>
                                     {(provided, snapshot) => (
                                         <div
                                             {...provided.droppableProps}
                                             ref={provided.innerRef}
-                                            className={`droppable ${snapshot.isDraggingOver ? 'isDraggingOver' : ''}`}
+                                            className={`droppable ${
+                                                snapshot.isDraggingOver ? 'isDraggingOver' : ''
+                                            }`}
                                         >
                                             {items.map((item, index) => (
-                                                <Draggable key={item} draggableId={item} index={index}>
+                                                <Draggable
+                                                    key={item}
+                                                    draggableId={item}
+                                                    index={index}
+                                                >
                                                     {(provided, snapshot) => (
                                                         <div
                                                             ref={provided.innerRef}
                                                             {...provided.draggableProps}
                                                             {...provided.dragHandleProps}
-                                                            className={`draggable ${snapshot.isDragging ? 'isDragging' : ''}`}
+                                                            className={`draggable ${
+                                                                snapshot.isDragging
+                                                                    ? 'isDragging'
+                                                                    : ''
+                                                            }`}
                                                         >
                                                             {item}
                                                         </div>
@@ -108,17 +172,33 @@ function ListReorder() {
                                     )}
                                 </Droppable>
                             </DragDropContext>
+                            {/* Toon foutmelding onder de vraag als deze fout is beantwoord */}
+                            {errors[questionIndex] && (
+                                <p className="error">Dit antwoord is fout, probeer opnieuw!</p>
+                            )}
                         </div>
-                        <div className='arrow'>
-                            <div className='arrow-text-top'>{currentQuestion.options_measurement[0]}</div>
-                            <div className='arrow-top'></div>
-                            <div className='arrow-line'></div>
-                            <div className='arrow-bottom'></div>
-                            <div className='arrow-text-bottom'>{currentQuestion.options_measurement[1]}</div>
+                        <div className="arrow">
+                            <div className="arrow-text-top">
+                                {currentQuestion.options_measurement[0]}
+                            </div>
+                            <div className="arrow-top"></div>
+                            <div className="arrow-line"></div>
+                            <div className="arrow-bottom"></div>
+                            <div className="arrow-text-bottom">
+                                {currentQuestion.options_measurement[1]}
+                            </div>
                         </div>
                     </div>
                 );
             })}
+            <button
+                type="button"
+                onClick={checkAnswers}
+                className="btn-next-page"
+                disabled={!allAnswered}
+            >
+                Submit &#8594;
+            </button>
         </div>
     );
 }

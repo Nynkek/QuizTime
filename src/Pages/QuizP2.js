700e5fd0c71454cb62c561f5ecd1f5b395592a97
Pim <pim@pixelrock.nl>
2024-09-23 12:16:03 +0200

    Dankjewel Prettier!

--- src/Pages/QuizP2.js
+++ src/Pages/QuizP2.js
@@ -1,28 +1,20 @@
 import React, { useEffect } from 'react';
-import Header from "../Components/Header/Header";
+import Header from '../Components/Header/Header';
 import './Pages.css';
 import MultipleChoice from '../Components/Quiz/MultipleChoice';
 
-
-
 function QuizP2() {
-
     useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+        window.scrollTo(0, 0);
+    }, []);
 
     return (
         <>
             <Header />
-            < div className="content-page" >
-                <div className="content" >
-
-                    < MultipleChoice nextpage="/quiz2end" />
-
-
-
+            <div className="content-page">
+                <div className="content">
+                    <MultipleChoice nextpage="/quiz2end" />
                 </div>
-
             </div>
         </>
     );

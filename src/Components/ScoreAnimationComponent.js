7443379f5b4f70707ab3337e2634a031ab14089e
Nynke <35605149+Nynkek@users.noreply.github.com>
2024-09-25 16:04:33 +0200

    css

--- src/Components/ScoreAnimationComponent.js
+++ src/Components/ScoreAnimationComponent.js
@@ -1,39 +1,43 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { ScoreContext } from '../Context/ScoreProvider';
+import React, { useContext, useState, useEffect } from "react";
+import { ScoreContext } from "../Context/ScoreProvider";
 
 export default function ScoreAnimationComponent() {
-    const { score } = useContext(ScoreContext); // Gebruik de context
-    const [showsAnimation, setShowsAnimation] = useState(false);
+  const { score } = useContext(ScoreContext); // Gebruik de context
+  const [showsAnimation, setShowsAnimation] = useState(false);
 
-    const [latestScore, setLatestScore] = useState(() => score);
-    const [scoreDifference, setScoreDifference] = useState(0);
+  const [latestScore, setLatestScore] = useState(() => score);
+  const [scoreDifference, setScoreDifference] = useState(0);
 
+  useEffect(() => {
+    if (score === latestScore) {
+      return;
+    }
+    setScoreDifference(score - latestScore);
+    setLatestScore(score);
 
+    setShowsAnimation(true);
+    const timeout = setTimeout(() => {
+      setShowsAnimation(false);
+    }, 1000);
 
-    useEffect(() => {
-        if (score === latestScore) {
-            return;
-        }
-        setScoreDifference(score - latestScore);
-        setLatestScore(score);
-
-        setShowsAnimation(true);
-        const timeout = setTimeout(() => {
-            setShowsAnimation(false);
-        }, 1000);
-
-        return () => clearTimeout(timeout);
+    return () => clearTimeout(timeout);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [score]);
+  }, [score]);
 
-    const isPositive = scoreDifference > 0;
-   
+  const isPositive = scoreDifference > 0;
 
-    return (
-        <div className={`animation-overlay ${showsAnimation ? 'visible' : ''}`}>
-            <div className={`score-container ${isPositive ? 'positive' : 'negative'}`}>
-                <h1>{scoreDifference}</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`
+        animation-overlay 
+        ${showsAnimation ? "visible" : ""} 
+        `}
+    >
+      <div
+        className={`score-container ${isPositive ? "positive" : "negative"}`}
+      >
+        <h1>{scoreDifference}</h1>
+      </div>
+    </div>
+  );
 }
--- src/Pages/WelcomeTeam.js
+++ src/Pages/WelcomeTeam.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './Pages.css';
-
-
+import "./Pages.css";
 
 function WelcomeTeam() {
-    const [selectedTeam, setSelectedTeam] = useState("");
+  const [selectedTeam, setSelectedTeam] = useState("");
 
-    useEffect(() => {
-        const savedTeam = localStorage.getItem("selectedTeam");
-        if (savedTeam) {
-            setSelectedTeam(savedTeam);
-        }
-    }, []);
+  useEffect(() => {
+    const savedTeam = localStorage.getItem("selectedTeam");
+    if (savedTeam) {
+      setSelectedTeam(savedTeam);
+    }
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    return (
+  return (
+    <>
+      <div className="content-page">
+        <h1 className="circle-bg page-title">Welkom {selectedTeam}!</h1>
+        <div className="content">
+          <ol>
+            <li>Gebruik steeds dezelfde telefoon voor de quiz</li>
+            <li>Beantwoord de vragen samen met je team</li>
+            <li>
+              Je score wordt bijgehouden, dus denk goed na over de antwoorden
+            </li>
+            <li>
+              Je mag de volgende envelop pas openen als je het codewoord hebt
+            </li>
+            <li>
+              De route is in totaal zo'n 2km lopen, het avontuur duurt ongeveer
+              een uur.
+            </li>
 
-        <>
-            <div className='content-page'>
-                <h1 className='circle-bg'>Welkom {selectedTeam}!</h1>
-                <div className='content'>
-                    <ol>
-                        <li>Gebruik steeds dezelfde telefoon voor de quiz
-                        </li>
-                        <li>Beantwoord de vragen samen met je team
-                        </li>
-                        <li>Je score wordt bijgehouden, dus denk goed na over de antwoorden</li>
-                        <li>Je mag de volgende envelop pas openen als je het codewoord hebt</li>
-                        <li>De route is in totaal zo'n 2km lopen, het avontuur duurt ongeveer een uur.</li>
-
-                        <li>Bij vragen kun je Nynke een bericht sturen <a href="https://wa.me/+31648813006">06 48813006</a></li>
-                    </ol>
-
-                </div>
-                <Link to="/quiz1" className='link-next-page'>
-
-                    <button type='button' className='btn-next-page'>Start quiz &#8594; </button>
-                </Link>
-            </div>
-
-        </>
-    );
+            <li>
+              Bij vragen kun je Nynke een bericht sturen{" "}
+              <a href="https://wa.me/+31648813006">06 48813006</a>
+            </li>
+          </ol>
+        </div>
+        <Link to="/quiz1" className="link-next-page">
+          <button type="button" className="btn-next-page">
+            Start quiz &#8594;{" "}
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default WelcomeTeam;

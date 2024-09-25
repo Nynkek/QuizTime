b6a348998088d37eff098e0852ddf0a60d5af925
Nynke <35605149+Nynkek@users.noreply.github.com>
2024-09-25 18:03:17 +0200

    draggable icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { ScoreContext } from '../../Context/ScoreProvider';
+import React, { useState, useEffect, useContext } from "react";
+import { ScoreContext } from "../../Context/ScoreProvider";
 
 import "./header.css";
 
 function Header() {
-    const [selectedTeam, setSelectedTeam] = useState("");
-    const { score } = useContext(ScoreContext); // Gebruik de context
+  const [selectedTeam, setSelectedTeam] = useState("");
+  const { score } = useContext(ScoreContext); // Gebruik de context
 
-    useEffect(() => {
-        const savedTeam = localStorage.getItem("selectedTeam");
-        if (savedTeam) {
-            setSelectedTeam(savedTeam);
-        }
-    }, []);
+  useEffect(() => {
+    const savedTeam = localStorage.getItem("selectedTeam");
+    if (savedTeam) {
+      setSelectedTeam(savedTeam);
+    }
+  }, []);
 
-    return (
-        <>
-            <header className="header">
-                <div className="team-name">
-                    <h3>{selectedTeam}</h3>
-                    <p>vraag 1</p>
-                </div>
-                <div className="score">
-                    Score: {score}
-                </div>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className="header">
+        <div className="team-name">
+          <h3>{selectedTeam}</h3>
+          {/* <p>vraag 1</p> */}
+        </div>
+        <div className="score">
+          <p>Score:</p> <p>{score}</p>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
